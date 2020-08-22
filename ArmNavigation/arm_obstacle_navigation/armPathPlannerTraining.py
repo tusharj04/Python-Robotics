aef53358f173ce6d02e937e00747459af42e8a82
ArkhamKnight9103 <nihal.bhatnagar@gmail.com>
2020-08-22 14:58:32 -0500

    sasasas

--- ArmNavigation/arm_obstacle_navigation/armPathPlannerTraining.py
+++ ArmNavigation/arm_obstacle_navigation/armPathPlannerTraining.py
@@ -1,10 +1,12 @@
-
-import keras
-from keras.preprocessing.image import ImageDataGenerator
-from keras.layers import Input, Conv2D, Dropout
-from keras.layers.normalization import BatchNormalization
-from keras.models import Model, load_model
-from keras.callbacks import EarlyStopping, ModelCheckpoint
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Conv2D, Flatten, Dense, Dropout
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
 import numpy as np
 
@@ -17,32 +19,43 @@ train_datagen = ImageDataGenerator(
         shear_range=0.2,
         zoom_range=0.2,
         horizontal_flip=True)
-training_set_x = train_datagen.flow_from_directory(
-    '/Users/prana/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/training_set_x',
+training_set_x = tf.keras.preprocessing.image_dataset_from_directory(
+    directory = #'/Users/prana/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/training_set_x',
     #'/Users/palluri/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/training_set_x',
-    #'C:\\Users\\nihal\\Documents\\GitHub\\PythonRobotics\\ArmNavigation\\arm_obstacle_navigation\\training_set_x',
-        target_size=(100, 100), #need to decide whether we plan to use 100x100 data size for the images cuz its gonna take a long time to train then
-        class_mode='binary') #what type of class mode is it
-training_set_y = train_datagen.flow_from_directory(
-    '/Users/prana/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/training_set_y',
+    'C:\\Users\\nihal\\Documents\\GitHub\\PythonRobotics\\ArmNavigation\\arm_obstacle_navigation\\training_set_x',
+    labels='inferred',
+    label_mode='categorical',
+    batch_size=32,
+    image_size=(256, 256))
+training_set_y = tf.keras.preprocessing.image_dataset_from_directory(
+    directory = #'/Users/prana/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/training_set_y',
     #'/Users/palluri/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/training_set_y',
-    #'C:\\Users\\nihal\\Documents\\GitHub\\PythonRobotics\ArmNavigation\\arm_obstacle_navigation\\training_set_y',
-        class_mode='binary') #what type of class mode is it
+    'C:\\Users\\nihal\\Documents\\GitHub\\PythonRobotics\ArmNavigation\\arm_obstacle_navigation\\training_set_y',
+    labels='inferred',
+    label_mode='categorical',
+    batch_size=32,
+    image_size=(256, 256))#what type of class mode is it
 
 #Preprocessing the test set
 test_datagen = ImageDataGenerator(rescale=1./255)
-test_set_x = test_datagen.flow_from_directory(
-    '/Users/prana/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_x',
+test_set_x = tf.keras.preprocessing.image_dataset_from_directory(
+    directory = #'/Users/prana/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_x',
+    'C:\\Users\\nihal\\Documents\\GitHub\\PythonRobotics\ArmNavigation\\arm_obstacle_navigation\\test_set_x',
     #'/Users/palluri/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_x',
     # 'arm_obstacle_navigation/test_set_x',
-        target_size=(100, 100), #also need to decide the images here
-        class_mode='binary') #need to determine the proper class mode
-test_set_y = test_datagen.flow_from_directory(
-    '/Users/prana/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_y',
+    labels='inferred',
+    label_mode='categorical',
+    batch_size=32,
+    image_size=(256, 256))
+test_set_y = tf.keras.preprocessing.image_dataset_from_directory(
+    directory = #'/Users/prana/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_y',
+    'C:\\Users\\nihal\\Documents\\GitHub\\PythonRobotics\ArmNavigation\\arm_obstacle_navigation\\test_set_y',
     #'/Users/palluri/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_y',
     #'arm_obstacle_navigation/test_set_y',
-        target_size=(100, 100), #also need to decide the images here
-        class_mode='binary') #need to determine the proper class mode
+    labels='inferred',
+    label_mode='categorical',
+    batch_size=32,
+    image_size=(256, 256))
 #i am not sure if the images are already 100 by 100, but if they are, i think we do not even need most of the above stuff,
 
 #Initializing the CNN
@@ -73,7 +86,7 @@ model.compile(optimizer='adam',loss='mse',metrics=['accuracy'])
 
 #model.fit(training_set_x.reshape(10000, 100, 100, 3), training_set_y.reshape(10000,100,100,1), batch_size=64, validation_split=1/14, epochs=1000, verbose=1, callbacks=[early_stop, save_weights])
 #model.fit_generator(generator, epochs=int, steps_per_epoch=int, validation_data=tuple, validation_steps=int)
-model.fit(x = training_set_x, y = training_set_y, validation_split=1/14, epochs=1000, verbose=1, callbacks=[early_stop, save_weights])
+model.fit(training_set_x, training_set_y, validation_data=(test_set_x, test_set_y), epochs=1000, verbose=1, callbacks=[early_stop, save_weights])
 #we are fitting to trainng set, 10000 sets a workspace + start grid + final grid (3 total) which are all 100 by 100 each
 print('Save trained model ...')
 model.load_weights('weights_2d.hf5')
