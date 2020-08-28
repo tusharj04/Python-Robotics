d5788ac4ac54920f46da496d77df334fc98442fe
ArkhamKnight9103 <nihal.bhatnagar@gmail.com>
2020-08-27 20:55:01 -0500

    gaga

--- ArmNavigation/arm_obstacle_navigation/Datageneration.py
+++ ArmNavigation/arm_obstacle_navigation/Datageneration.py
@@ -14,6 +14,117 @@ import sys
 
 
 import numpy as np
+class ListNode:
+    """
+    A node in a singly-linked list.
+    """
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+    def __repr__(self):
+        return repr(self.data)
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        """
+        Create a new singly-linked list.
+        Takes O(1) time.
+        """
+        self.head = None
+        self.size = 0;
+
+    def __repr__(self):
+        """
+        Return a string representation of the list.
+        Takes O(n) time.
+        """
+        nodes = []
+        curr = self.head
+        while curr:
+            nodes.append(repr(curr))
+            curr = curr.next
+        return '[' + ', '.join(nodes) + ']'
+
+    def prepend(self, data):
+        """
+        Insert a new element at the beginning of the list.
+        Takes O(1) time.
+        """
+        self.head = ListNode(data=data, next=self.head)
+        self.size++
+
+    def append(self, data):
+        """
+        Insert a new element at the end of the list.
+        Takes O(n) time.
+        """
+        if not self.head:
+            self.head = ListNode(data=data)
+            return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = ListNode(data=data)
+        self.size++
+
+    def find(self, key):
+        """
+        Search for the first element with `data` matching
+        `key`. Return the element or `None` if not found.
+        Takes O(n) time.
+        """
+        curr = self.head
+        while curr and curr.data != key:
+            curr = curr.next
+        return curr  # Will be None if not found
+
+    def remove(self, key):
+        """
+        Remove the first occurrence of `key` in the list.
+        Takes O(n) time.
+        """
+        # Find the element and keep a
+        # reference to the element preceding it
+        curr = self.head
+        prev = None
+        while curr and curr.data != key:
+            prev = curr
+            curr = curr.next
+        # Unlink it from the list
+        if prev is None:
+            self.head = curr.next
+        elif curr:
+            prev.next = curr.next
+            curr.next = None
+        self.size--
+    def dequeue(self):
+        """
+        Dequeues first element in list
+        """
+        # Find the element and keep a
+        # reference to the element preceding it
+        curr = self.head
+        self.head = curr.next
+        prev = None
+        self.size--
+        return curr.data
+
+    def reverse(self):
+        """
+        Reverse the list in-place.
+        Takes O(n) time.
+        """
+        curr = self.head
+        prev_node = None
+        next_node = None
+        while curr:
+            next_node = curr.next
+            curr.next = prev_node
+            prev_node = curr
+            curr = next_node
+        self.head = prev_node
 np.set_printoptions(threshold=sys.maxsize)
 
 #Preprocessing the training set
@@ -74,13 +185,11 @@ print('Saving images in array format...')
 
 
 # convert to numpy array
-#FinalArmConfigImageArray = []
-FinalArmConfigImageArray = np.array(FinalArmConfigImageArray, dtype=np.int8)
+FinalArmConfigImageArray = SinglyLinkedList()
 for x in range (9999):
     #img = load_img('/Users/palluri/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/training_set_x/finalarmconfig/finalarmconfig{:05d}.png'.format(x))
     img = load_img('/Users/prana/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/training_set_x/finalarmconfig/finalarmconfig{:05d}.png'.format(x))
     FinalArmConfigImageArray.append(img_to_array(img))
-    #FinalArmConfigImageArray = FinalArmConfigImageArray.astype(np.int32)
     print(x)
     #np.savetxt('FinalArmConfigImageArray.dat', FinalArmConfigImageArray)
     #fullarray = (img_to_array(img))
@@ -94,15 +203,17 @@ for x in range (9999):
     #FinalArmConfigImageArray.append(img_to_array(img))
 #f=open("FinalArmConfigImageArray", "a+")
 #np.savetxt('FinalArmConfigImageArray.dat', FinalArmConfigImageArray)
-
-
-
-WorkSpaceImageArray = []
+holderArray = []
+for c in range(FinalArmConfigImageArray.size):
+    holderArray[c] = FinalArmConfigImageArray.dequeue()
+print(holderArray)
+WorkSpaceImageArray =  SinglyLinkedList()
 for x in range (9999):
     #img = load_img('/Users/palluri/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/training_set_x/workspacegrid/workspace{:05d}.png'.format(x))
     img = load_img('/Users/prana/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/training_set_x/workspacegrid/workspace{:05d}.png'.format(x))
     WorkSpaceImageArray.append(img_to_array(img))
     print(x)
+
     #fullarray = (img_to_array(img))
     #print(fullarray)
     #reshapedarray = fullarray.reshape(100,100)
@@ -115,7 +226,7 @@ for x in range (9999):
     #f.close()
 
 
-StartArmConfigImageArray = []
+StartArmConfigImageArray =  SinglyLinkedList()
 for x in range (9999):
     img = load_img#('/Users/palluri/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/training_set_x/startarmconfig/startarmconfig{:05d}.png'.format(x))
     ('/Users/prana/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/training_set_x/startarmconfig/startarmconfig{:05d}.png'.format(x))
@@ -133,7 +244,7 @@ for x in range (9999):
     #f.write(shortenedarray)
     #f.close()
 
-routeGridImageArray = []
+routeGridImageArray = SinglyLinkedList()
 for x in range (10000):
     #img = load_img('/Users/palluri/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/training_set_y/routegrid/route{:05d}.png'.format(x))
     img = load_img('/Users/prana/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/training_set_y/routegrid/route{:05d}.png'.format(x))
@@ -161,7 +272,7 @@ trainingDataXArray.append(FinalArmConfigImageArray)
 trainingDataXArray.append(WorkSpaceImageArray)
 trainingDataXArray.append(StartArmConfigImageArray)
 
-FinalArmConfigImageArrayTest = []
+FinalArmConfigImageArrayTest = SinglyLinkedList()
 for x in range (999):
     #img = load_img('/Users/palluri/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_x/testsetfinalarmconfig/finalarmconfig{:05d}.png'.format(x)))
     img = load_img('/Users/prana/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_x/testsetfinalarmconfig/finalarmconfig{:05d}.png'.format(x))
@@ -180,11 +291,11 @@ for x in range (999):
     #f.close()
 
 
-FinalWorkSpaceImageArrayTest = []
+FinalWorkSpaceImageArrayTest =  SinglyLinkedList()
 for x in range (999):
-    #img = load_img('/Users/palluri/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_x/testsetworkspace/workspace{:05d}.png'.format(x))
+    img = load_img('/Users/palluri/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_x/testsetworkspace/workspace{:05d}.png'.format(x))
     #FinalWorkSpaceImageArrayTest.append(img_to_array(img)))
-    img = load_img('/Users/prana/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_x/testsetworkspace/workspace{:05d}.png'.format(x))
+    #img = load_img('/Users/prana/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_x/testsetworkspace/workspace{:05d}.png'.format(x))
     #('/Users/palluri/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_x/testsetworkspace/workspace{:05d}.png'.format(x))
     FinalWorkSpaceImageArrayTest.append(img_to_array(img))
     #FinalWorkSpaceImageArrayTest.append(img_to_array(img))
@@ -200,10 +311,10 @@ for x in range (999):
     #f.close()
 
 
-StartArmConfigImageArrayTest = []
+StartArmConfigImageArrayTest = SinglyLinkedList()
 for x in range (999):
-    #img = load_img('/Users/palluri/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_x/testsetstartarmconfig/startarmconfig{:05d}.png'.format(x))
-    img = load_img('/Users/prana/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_x/testsetstartarmconfig/startarmconfig{:05d}.png'.format(x))
+    img = load_img('/Users/palluri/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_x/testsetstartarmconfig/startarmconfig{:05d}.png'.format(x))
+    #img = load_img('/Users/prana/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_x/testsetstartarmconfig/startarmconfig{:05d}.png'.format(x))
     #('/Users/palluri/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_x/testsetstartarmconfig/startarmconfig{:05d}.png'.format(x))
     StartArmConfigImageArrayTest.append(img_to_array(img))
     #StartArmConfigImageArrayTest.append(img_to_array(img))
@@ -218,10 +329,10 @@ for x in range (999):
     #f.write(shortenedarray)
     #f.close()
 
-routeGridImageArrayTest = []
+routeGridImageArrayTest = SinglyLinkedList()
 for x in range (1000):
     #img = load_img('/Users/palluri/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_y/testsetroute/route{:05d}.png'.format(x))
-    img = load_img('/Users/prana/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_y/testsetroute/route{:05d}.png'.format(x))
+    #img = load_img('/Users/prana/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_y/testsetroute/route{:05d}.png'.format(x))
     #('/Users/palluri/Documents/GitHub/PythonRobotics/ArmNavigation/arm_obstacle_navigation/test_set_y/testsetroute/route{:05d}.png'.format(x))
     routeGridImageArrayTest.append(img_to_array(img))
     #routeGridImageArrayTest.append(img_to_array(img))
